94d4c4daffdb7dec9211d5de110d02ddb89001cd
Brayan Henao <darbra18@gmail.com>
2024-11-19 17:46:20 +0100

    ajoute de costume dans la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,17 +20,17 @@ user3 = User.create(username: "bob_ross", email: "bob@example.com", password: "a
 
 
 file = URI.parse("https://images.pexels.com/photos/28830053/pexels-photo-28830053/free-photo-of-pirata-historico-en-un-gran-velero-usando-un-telescopio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1").open
-costume1 = Costume.new(title: "Pirate" , description: "This pirate costume includes the shirt-like t-shirt and vest that are one, a bandana, a panty hose, a belt and overboots", price: 20.0, owner: user2 )
+costume1 = Costume.new(title: "Pirate" , description: "This pirate costume includes the shirt-like t-shirt and vest that are one, a bandana, a panty hose, a belt and overboots", price: 20, owner: user2 )
 costume1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 costume1.save
 
 file = URI.parse("https://img.freepik.com/premium-photo/batman-superhero-limited-edition-costume_947926-952.jpg").open
-costume2 = Costume.new(title: "superhero", description: "Batman costume for adults, in size L. Includes: padded suit with matching boot covers, cape and mask of Superheroe.", price: 25.0, owner: user1)
+costume2 = Costume.new(title: "superhero", description: "Batman costume for adults, in size L. Includes: padded suit with matching boot covers, cape and mask of Superheroe.", price: 25, owner: user1)
 costume2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 costume2.save
 
 file = URI.parse("https://sc04.alicdn.com/kf/H3ba607eaa6a0444d8a397ff8186aae0cq.jpg").open
-costume3 = Costume.new(title: "Predator", description: "Authentic Predator Design: This life-size realistic robot costume predator cosplay costume is designed to look like the iconic alien hunter from the Predator franchise, perfect for movie fans.", price: 125.0, owner: user3)
+costume3 = Costume.new(title: "Predator", description: "Authentic Predator Design: This life-size realistic robot costume predator cosplay costume is designed to look like the iconic alien hunter from the Predator franchise, perfect for movie fans.", price: 125, owner: user3)
 costume3.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 costume3.save
 
@@ -38,9 +38,24 @@ file = URI.parse("https://i.pinimg.com/564x/7c/66/21/7c66216e03c8060ca67f0f476d5
 costume4 = Costume.new(title: "The Mandalorian", description: " Armor: Made with high-quality materials, the armor is durable and comfortable, allowing for easy movement.
                                                                 Helmet: Designed to resemble the helmet of the main character, Din Djarin, with precise details and an ergonomic design.
                                                                 Cape: A long and wide cape that adds a dramatic touch to the ensemble.
-                                                                Accessories: Includes a blaster (laser gun) and other accessories to complete the look.", price: 150.0, owner: user1)
+                                                                Accessories: Includes a blaster (laser gun) and other accessories to complete the look.", price: 150, owner: user1)
 costume4.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 costume4.save
+
+file = URI.parse("https://cdn.vegaoo.es/images/rep_art/gra/321/3/321385/disfraz-de-pareja-asterix-y-obelix-adulto.jpg").open
+costume5 = Costume.new(title: "Asterix & Obelix", description: "Funny costumes of asterix and obelix to be the center of attention of the party", price: 30, owner: user3)
+costume5.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+costume5.save
+
+file = URI.parse("https://www.idealfiestas.com/img/cms/Carnaval%20Ideal%20fiestas/Mujer%20disfraz/Disfraces%20pirata%20500x500.jpg").open
+costume6 = Costume.new(title: "pirate woman", description: "Pirate costume , ideal for carnival and theme parties. Dress up your pirate costume and board the black pearl to sail the 7 seas to search for lost treasures.", price: 25, owner: user2)
+costume6.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+costume6.save
+
+file = URI.parse("https://img.fruugo.com/product/8/43/194235438_max.jpg").open
+costume7 = Costume.new(title: "Maleficent ", description: " You can be Maleficent, the Lady of all evil from Disney is 2014 live action fantasy horror with Angelina Jolie. Beware of princess Aurora", price: 25, owner: user1)
+costume7.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+costume7.save
 
 booking1 = Booking.create!(start_date: Date.today, end_date: Date.today + 3, total_price: 60.0, user: user1, costume: costume1, status: "accepted")
 booking2 = Booking.create(start_date: Date.today + 5, end_date: Date.today + 7, total_price: 50.0, user: user2, costume: costume2, status: "pending")
