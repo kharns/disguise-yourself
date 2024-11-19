ad4a532dbef435faa45bb440e2376ad7ffa75251
kristen harnois <kristen.harnois@gmail.com>
2024-11-19 15:07:29 +0100

    changed user_id to owner_id in costumes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_113030) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_135620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,10 +60,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_113030) do
     t.string "title"
     t.string "description"
     t.float "price"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_costumes_on_user_id"
+    t.bigint "owner_id", null: false
+    t.index ["owner_id"], name: "index_costumes_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -83,5 +83,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_113030) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "costumes"
   add_foreign_key "bookings", "users"
-  add_foreign_key "costumes", "users"
+  add_foreign_key "costumes", "users", column: "owner_id"
 end
