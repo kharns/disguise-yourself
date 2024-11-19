a37bfec27d338aabd06cf2fb8c9e0bb5d5b9f78c
Brayan Henao <darbra18@gmail.com>
2024-11-19 16:04:45 +0100

    cle cloudinary ajoute

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,6 +3,6 @@ class Costume < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
 
-  validates :price, :photo_url, :description, :title, presence: true
+  validates :price, :photo, :description, :title, presence: true
   validates :title, length: { minimum: 5 }
 end
