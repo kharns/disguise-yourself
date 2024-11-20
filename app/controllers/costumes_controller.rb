8ae7233b05c28208e867414c6fdc7ac5a48b7480
kristen harnois <kristen.harnois@gmail.com>
2024-11-20 15:35:41 +0100

    show ok with booking option and bookings list

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,9 +1,11 @@
 class CostumesController < ApplicationController
-  def show
-    # @costume = Costume.find(params[:id])
-  end
-
   def index
     @costumes = Costume.all
+  end
+
+  def show
+    @costume = Costume.find(params[:id])
+    @booking = Booking.new
+    @user_bookings = @costume.bookings.where(user: current_user).sort_by(&:start_date)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,16 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, :total_price, :costume, :status, presence: true
   validates :status, inclusion: { in: %w[pending declined accepted] }
+
+  validate :start_date_cannot_be_in_the_past, :end_date_must_be_after_start
+
+  def start_date_cannot_be_in_the_past
+    errors.add(:start_date, "can't be in the past") if
+      start_date < Date.today
+  end
+
+  def end_date_must_be_after_start
+    errors.add(:end_date, "must be after start date") if
+      end_date <= start_date
+  end
 end
