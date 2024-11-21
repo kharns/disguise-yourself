e1e7da0707da75f0cc1a90cae9ed52b7c95520ed
Maxime Lazzarotto <lazzarottomaxime@gmail.com>
2024-11-21 13:22:12 +0100

    creation du dashboard avec booking user et owner + collection des costumes owner en vente

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,10 @@ class PagesController < ApplicationController
     @costume1 = @costumes.sample
     @costume2 = @costumes.sample
   end
+
+  def dashboard
+    @user = current_user
+    @user_bookings = @user.bookings
+    @owner_bookings = Booking.joins(:costume).where(costumes: { owner_id: current_user.id })
+  end
 end
