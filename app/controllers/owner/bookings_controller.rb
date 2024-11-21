2557d0506359dd918f67e41a037c4b082705bf10
kristen harnois <kristen.harnois@gmail.com>
2024-11-21 13:21:32 +0100

    functions accept and decline booking working

---
+++ app/controllers/owner/bookings_controller.rb
@@ -0,0 +1,18 @@
+class Owner::BookingsController < ApplicationController
+
+  # ACCEPTER BOOKING
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepted"
+    @booking.save
+    redirect_to costume_path(@booking.costume)
+  end
+
+  # REFUSER BOOKING
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.status = "decline"
+    @booking.save
+    redirect_to costume_path(@booking.costume)
+  end
+end
